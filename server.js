9fa7e311c0afcfad1dbc324ea6b8b09ee17edd05
Rajeshwar <rchennuri83@gmail.com>
2016-08-02 08:16:06 +0530

    Update server.js
    This is for git practise

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
+//this is for git practice
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
